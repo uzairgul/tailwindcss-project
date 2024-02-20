741008fd6abdaf8fbd1e734c2683164ec5ca19a5
uzairgul <uzairgul2000@gmail.com>
2024-02-20 14:27:29 +0500

    first

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily : {
+      Inter: ['Inter', "san-serif"],
+      Poppins : ['Poppins']
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
